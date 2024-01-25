785029e4b107084a5c2a6e047b188e8d2be2fa35
Olan Gomez <johnolansaldivar.gomez@bicol-u.edu.ph>
2024-01-25 18:11:39 +0800

    feat(main): Added button state change
    When each die is pressed, the number on it will change.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@
  */
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -26,7 +27,18 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  //create variable
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,18 +48,28 @@ class DicePage extends StatelessWidget {
             child: TextButton(
               //onPressed function - what to do when button gets pressed
               onPressed: () {
-                print("Left button got pressed");
+                //see what changed, update, and redraws the entire screen
+                setState(() {
+                  //Random().nextInt(6) - generates numbers from 0-5
+                  //Random().nextInt(6) + 1 - generates numbers from 1-6
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             flex: 1,
             child: TextButton(
               onPressed: () {
-                print("Right button got pressed");
+                //see what changed, update, and redraws the entire screen
+                setState(() {
+                  //Random().nextInt(6) - generates numbers from 0-5
+                  //Random().nextInt(6) + 1 - generates numbers from 1-6
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
